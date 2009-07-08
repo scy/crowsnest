d15cb1bbd67ad5e7c6ec7dba2a9e33d910f49169
Tim Weber <scy-piraten@scytale.name>
2009-07-08 18:50:58 +0200

    Explicitly pull/push „origin master“.

--- bin/cron.sh
+++ bin/cron.sh
@@ -4,9 +4,9 @@ d="$(date --rfc-3339=seconds)"
 
 cd "$(dirname "$0")"
 cd ..
-git pull
+git pull origin master
 cd bin
 ./update.sh
 git add .
 git commit -m "Automatic update $d."
-git push
+git push origin master
